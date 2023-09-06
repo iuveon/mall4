3abb89318ff75da911d247b92b78118722c6f7f3
Jo Juyeon <ioiuveon@gmail.com>
2023-09-06 13:20:45 +0900

    re setting

--- mall4/target/classes/t_member.sql
+++ mall4/target/classes/t_member.sql
@@ -16,6 +16,20 @@ create table t_member(
 	m_regdate date default sysdate not null -- 가입일
 );
 
-alter table t_member add constraint pk_member primary key (m_id);
+alter table t_member add constraint pk_member primary key (m_num);
 
 drop table t_member;
+
+select * from T_MEMBER;
+
+insert into t_member (m_num, m_id, m_pw, m_name, m_email1, m_email2, m_phone) values (seq_member_num.nextval, 'testid1', '1234', '테스터', 'tester', 'ez.com', '010-0000-0001');
+insert into t_member (m_num, m_id, m_pw, m_name, m_email1, m_email2, m_phone) values (seq_member_num.nextval, 'testid2', '1234', '테스터', 'tester', 'ez.com', '010-0000-0002');
+insert into t_member (m_num, m_id, m_pw, m_name, m_email1, m_email2, m_phone) values (seq_member_num.nextval, 'testid3', '1234', '테스터', 'tester', 'ez.com', '010-0000-0003');
+insert into t_member (m_num, m_id, m_pw, m_name, m_email1, m_email2, m_phone) values (seq_member_num.nextval, 'testid4', '1234', '테스터', 'tester', 'ez.com', '010-0000-0004');
+insert into t_member (m_num, m_id, m_pw, m_name, m_email1, m_email2, m_phone) values (seq_member_num.nextval, 'testid5', '1234', '테스터', 'tester', 'ez.com', '010-0000-0005');
+insert into t_member (m_num, m_id, m_pw, m_name, m_email1, m_email2, m_phone) values (seq_member_num.nextval, 'testid6', '1234', '테스터', 'tester', 'ez.com', '010-0000-0006');
+insert into t_member (m_num, m_id, m_pw, m_name, m_email1, m_email2, m_phone) values (seq_member_num.nextval, 'testid7', '1234', '테스터', 'tester', 'ez.com', '010-0000-0007');
+insert into t_member (m_num, m_id, m_pw, m_name, m_email1, m_email2, m_phone) values (seq_member_num.nextval, 'testid8', '1234', '테스터', 'tester', 'ez.com', '010-0000-0008');
+insert into t_member (m_num, m_id, m_pw, m_name, m_email1, m_email2, m_phone) values (seq_member_num.nextval, 'testid9', '1234', '테스터', 'tester', 'ez.com', '010-0000-0009');
+insert into t_member (m_num, m_id, m_pw, m_name, m_email1, m_email2, m_phone) values (seq_member_num.nextval, 'testid10', '1234', '테스터', 'tester', 'ez.com', '010-0000-0010');
+insert into t_member values (seq_member_num.nextval, 'testid11', '1234', '테스터', 'tester', 'ez.com', '010-0000-0011', '19920101', '12345', '경기도 수원시 매산로', '경기도 수원시 매산동', 'EZ304', sysdate);
